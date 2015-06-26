3d071a8dad24ec29ff1be7e41dd0dc1545476793
Robert Starbuck <robstarbuck@gmail.com>
2015-06-26 18:47:20 +0100

    Initial Commit.

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name affixalllApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the affixalllApp
+ */
+angular.module('affixalllApp')
+  .controller('MainCtrl', function ($scope) {
+
+  	// alert('io');
+
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
